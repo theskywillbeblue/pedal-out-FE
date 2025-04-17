c7ba505187fcea5c4f870c9391d768ca5d68eef7
Sophie King <102487877+theskywillbeblue@users.noreply.github.com>
2025-04-17 13:27:09 +0100

    update UI to accept friends data when fetched

--- pedalout/app/(tabs)/FriendsScreen.tsx
+++ pedalout/app/(tabs)/FriendsScreen.tsx
@@ -1,97 +1,121 @@
-import { StyleSheet, View, SafeAreaView, Text } from 'react-native';
-import { useEffect } from 'react';
+import { StyleSheet, View, SafeAreaView, Text, ScrollView } from 'react-native';
+import { useEffect, useState } from 'react';
 import FloatingSearchBar from '../../components/search';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
-import {getFriends} from'../../api.js'
+import { getFriends } from '../../api.js';
 
 export default function TabFiveScreen() {
+  // const [friends, setFriends] = useState([]);
+  // const [isLoading, setLoading] = useState(true);
+  // const [error, setError] = useState(null);
+  // useEffect(() => {
+  //   getFriends()
+  //     .then((res) => {
+  //       setFriends(res.friends);
+  //     })
+  //     .catch((error) => {
+  //       setError(error);
+  //     })
+  //     .finally(() => {
+  //       setLoading(false);
+  //     });
+  // }, []);
 
-  const [friends, setFriends] = useState([]);
-  const [isLoading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    getFriends()
-      .then((res) => {
-        setFriends(res.friends);
-      })
-      .catch((error) => {
-        setError(error);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, []);
-
-  if (isLoading) {
-    return <Text>Fetching your friends...</Text>;
-  }
-  if (error) {
-    return <Text>Houston, we have a problem!</Text>;
-  }
-
-
+  // if (isLoading) {
+  //   return <Text>Fetching your friends...</Text>;
+  // }
+  // if (error) {
+  //   return <Text>Houston, we have a problem!</Text>;
+  // }
 
   return (
- <SafeAreaView style={{ flex: 1, paddingTop: 100 }}>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View>
+        <FloatingSearchBar />
+      </View>
 
-  <View style={styles.floatingWrapper}>
-    <FloatingSearchBar />
-  </View>
-
-
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
-        />
-      }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Placeholder Title</ThemedText>
+        <ThemedText type="title">Friends</ThemedText>
       </ThemedView>
-      <ThemedText>Placeholder Text</ThemedText>
-      
-    </ParallaxScrollView>
+      <ScrollView
+        vertical
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.avatarContainer}
+      >
+        <View style={styles.titleSpacer} />
+
+        <View style={styles.avatarRow}>
+          <View style={styles.avatarPlaceholder} />
+          <Text style={styles.avatarName}>Friend Name</Text>
+        </View>
+        <View style={styles.avatarRow}>
+          <View style={styles.avatarPlaceholder} />
+          <Text style={styles.avatarName}>Friend Name</Text>
+        </View>
+        <View style={styles.avatarRow}>
+          <View style={styles.avatarPlaceholder} />
+          <Text style={styles.avatarName}>Friend Name</Text>
+        </View>
+        <View style={styles.avatarRow}>
+          <View style={styles.avatarPlaceholder} />
+          <Text style={styles.avatarName}>Friend Name</Text>
+        </View>
+        <View style={styles.avatarRow}>
+          <View style={styles.avatarPlaceholder} />
+          <Text style={styles.avatarName}>Friend Name</Text>
+        </View>
+        <View style={styles.avatarRow}>
+          <View style={styles.avatarPlaceholder} />
+          <Text style={styles.avatarName}>Friend Name</Text>
+        </View>
+
+  
+      </ScrollView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
   titleContainer: {
+    paddingTop: 80,
+    paddingHorizontal: 16,
+    paddingBottom: 10,
+    paddingLeft: 23,
+    zIndex: 10,
+  },
+  titleSpacer: {
+    marginTop: 40,
+  },
+
+  title: {
+    fontFamily: 'HelveticaRoundedBold',
+    fontSize: 25,
+    lineHeight: 30,
+    marginBottom: 8,
+  },
+  avatarContainer: {
+    paddingHorizontal: 16,
+    flexDirection: 'column',
+    marginLeft: 15,
+    gap: 14,
+  },
+  avatarPlaceholder: {
+    width: 90,
+    height: 90,
+    borderRadius: 55,
+    backgroundColor: '#ccc',
+  },
+  avatarRow: {
     flexDirection: 'row',
-    gap: 8,
+    alignItems: 'center', 
+    gap: 20
   },
-  floatingWrapper: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    zIndex: 999, // make sure it's higher than anything else
-  },
-  floatingSearchContainer: {
-    marginTop: 23,
-    marginHorizontal: 20,
+  avatarName: {
     fontFamily: 'HelveticaRounded',
-    backgroundColor: 'white',
-    borderRadius: 12,
-    paddingHorizontal: 12,
-    paddingVertical: 12,
-    shadowColor: '#000000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 4,
-    elevation: 5,
+    fontSize: 20,
+    fontWeight: 700,
+    color: '#666',
   },
 });
