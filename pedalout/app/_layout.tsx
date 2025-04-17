9d63a44a269b22ba34a2f20b6254dd95f44e6cc2
Sophie King <102487877+theskywillbeblue@users.noreply.github.com>
2025-04-17 15:27:14 +0100

    update RideCard to route to RideDetails

--- pedalout/app/_layout.tsx
+++ pedalout/app/_layout.tsx
@@ -1,4 +1,8 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -12,18 +16,13 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     Helvetica: require('../assets/fonts/Helvetica.ttf'),
     HelveticaRoundedBold: require('../assets/fonts/helvetica-rounded-bold.otf'),
     HelveticaBold: require('../assets/fonts/Helvetica-Bold.ttf'),
-
-
   });
-
-  
 
   useEffect(() => {
     if (loaded) {
@@ -38,11 +37,10 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-
-        
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
-        <Stack.Screen name="MapScreen"/>
+        <Stack.Screen name="MapScreen" />
+        <Stack.Screen name="RideDetails" />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
