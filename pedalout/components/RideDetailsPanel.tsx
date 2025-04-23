4f5777be91dfbee3e8a60513f8efe0c452eeb68d
jaymulholland <jay.mulholland.7@gmail.com>
2025-04-23 15:13:44 +0100

    ridedetails changes

--- pedalout/components/RideDetailsPanel.tsx
+++ pedalout/components/RideDetailsPanel.tsx
@@ -1,65 +1,69 @@
 import React from 'react';
 import { View, Pressable, StyleSheet, Button } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 import { useThemeColor } from '@/hooks/useThemeColor';
 import { ThemedText } from './ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { router, useLocalSearchParams } from 'expo-router';
-
-
+import { ScreenHeight } from '@rneui/themed/dist/config';
+import { useState } from 'react';
+import TouchableOpacity from 'react-native-gesture-handler';
 
 function RideDetailsPanel() {
+  const [chatImages, setChatImages] = useState<string[]>([]);
+
   const { ride } = useLocalSearchParams();
-    const parsedRide = JSON.parse(ride as string);
+  const parsedRide = JSON.parse(ride as string);
   const backgroundColor = useThemeColor({}, 'background');
   const textColor = useThemeColor({}, 'text');
   const borderColor = useThemeColor({}, 'text');
-  const invertedBorderColor = useThemeColor({ light: '#000', dark: '#fff' }, 'text');
+  const invertedBorderColor = useThemeColor(
+    { light: '#000', dark: '#fff' },
+    'text',
+  );
 
   return (
-
-    
-
     <ThemedView style={styles.container}>
-      
-        <ThemedText style={[styles.title, { borderColor }]}>
-          {parsedRide.title}
-        </ThemedText>
+      <ThemedText style={[styles.title, { borderColor }]}>
+        {parsedRide.title}
+      </ThemedText>
 
-        <ThemedText style={[styles.desc2, { borderColor }]}>
-          address from geocode
-        </ThemedText>
+      <ThemedText style={[styles.desc2, { borderColor }]}>
+        address from geocode ????
+      </ThemedText>
 
-        <ThemedText style={[styles.desc2, { borderColor }]}>
-          {parsedRide.ride_date} {'\n'} {parsedRide.ride_time}
-        </ThemedText>
+      <ThemedText style={[styles.desc2, { borderColor }]}>
+        {parsedRide.ride_date} {'\n'} {parsedRide.ride_time}
+      </ThemedText>
 
-        <ThemedText style={[styles.desc2, { borderColor }]}>
-          Discipline: {parsedRide.ride_tags}
-        </ThemedText>
+      <ThemedText style={[styles.desc2, { borderColor }]}>
+        Discipline: {parsedRide.disciplines}
+      </ThemedText>
 
-        <View style={[styles.divider, {borderBottomColor: invertedBorderColor}]} />
+      <View
+        style={[styles.divider, { borderBottomColor: invertedBorderColor }]}
+      />
 
-        <ThemedText style={[styles.description, { borderColor }]}>
-          {parsedRide.description}
-        </ThemedText>
-        <ThemedText style={[styles.desc2, { borderColor }]}>
-          friends list from Chat
+      <ThemedText style={[styles.description, { borderColor }]}>
+        {parsedRide.description}
+      </ThemedText>
 
-        </ThemedText>
+      <ThemedText style={[styles.desc2, { borderColor }]}>
+        {parsedRide.participants.length} participants
+      </ThemedText>
 
-        <View style={styles.buttoncontainer}>
+      {/* <View style={styles.buttoncontainer}>
           <Pressable style={[styles.buttonplaceholder, { borderColor }]}>
             <ThemedText style={[styles.buttonplaceholdertext, { color: textColor }]}>
               useless button
             </ThemedText>
           </Pressable>
-        </View>
+        </View> */}
 
-        
+      <ThemedView style={styles.spaceContainer} />
 
-        <Button title="Join" />
-      </ThemedView>
-    
+      <Button title="Join" />
+    </ThemedView>
   );
 }
 
@@ -92,7 +96,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
   },
   divider: {
-    borderBottomWidth: 1,   // Thickness of the line
+    borderBottomWidth: 1, // Thickness of the line
     marginVertical: 12,
     width: '100%',
   },
@@ -113,14 +117,33 @@ const styles = StyleSheet.create({
   buttonplaceholdertext: {
     fontSize: 14,
   },
-  modalContainer: {
+  spaceContainer: {
     marginTop: 2,
+    height: 20,
   },
   modalContent: {
     borderRadius: 8,
     padding: 16,
   },
+  scrollOverlay: {
+    position: 'absolute',
+    top: 80,
+    left: 0,
+    right: 0,
+    zIndex: 10,
+    paddingVertical: 10,
+  },
+  avatarContainer: {
+    paddingHorizontal: 16,
+    flexDirection: 'row',
+    gap: 16,
+  },
+  avatarPlaceholder: {
+    width: 70,
+    height: 70,
+    borderRadius: 40,
+    backgroundColor: '#ccc',
+  },
 });
-
 
 export default RideDetailsPanel;
